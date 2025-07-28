e7ae9105c9f8d242e7675b4341c33f70dac629dd
SONALSUBODH <ms.sonalkulkarni@gmail.com>
2025-07-28 15:17:50 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,13 +1,27 @@
 variable "vpc_cidr" {
- type        = string
-  default    = ["10.0.0.0/16"]
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+
+variable "public_subnet_Cidr" {
+  type        = list(string)
+  description = "Public Subnet CIDR values"
+  default     = ["10.0.1.0/24" , "10.0.2.0/24"]
 }
 
 
 
 
-variable "public_subnet_cidrs" {
- type        = list
- description = "Public Subnet CIDR values"
- default     = ["10.0.1.0/24", "10.0.2.0/24"]
-}
+
+
+
+
+
+
+
+
+
+
+
+
